638c2f4abc18ad32b084561665bb83fbc3efd64d
Jack Bennett <73858124+j-ack-bennett@users.noreply.github.com>
2021-01-08 11:45:25 +1300

    Respond with a predefined file

--- server.js
+++ server.js
@@ -2,4 +2,15 @@ const express = require('express')
 
 const server = express()
 
+const PORT = 3000
+
+server.get('/compliment', (req, res) => {
+    res.send('<h1>Hello</h1><p>World</p>')
+})
+
+server.get('/profile', (req, res) => {
+    res.sendFile(__dirname + '/silvia.html')
+})
+
 module.exports = server
+
