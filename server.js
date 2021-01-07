0c42d57736411bbe587ac56cc9c425ff417deeee
Rosemary-Hawkes <15070793+Rosemary-Hawkes@users.noreply.github.com>
2021-01-08 12:07:43 +1300

    first two tasks completed

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express')
 
 const server = express()
+const port = 3000
+
 
 module.exports = server
+
+//Routes
+//http://localhost:3000/compliment
+server.get('/compliment', (request, response) => {
+  response.send('<h1>Hello there!</h1>')
+})
+
+//Routes
+//http://localhost:3000/profile
+server.get('/profile', (request, response) => {
+  response.sendFile(__dirname + '/silvia.html')
+})
