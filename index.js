dbbd782c997c30b1d7ec8fb11a648ef896607954
Michael Parkin <73506758+mike-parkin@users.noreply.github.com>
2021-01-08 11:28:16 +1300

    local server sends compliment

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
+const { response } = require('express')
 const server = require('./server')
 
 const port = process.env.PORT || 3000
+
+server.get('/', (request, response) => {
+  response.send('Hello')
+})
+
+server.get("/compliment", (req, res) => {
+  res.send('nice hair dude(non gender specific dude)')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 server.listen(port, function () {
   // eslint-disable-next-line no-console
