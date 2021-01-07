b7b995a9faaaf7460cc6c18d10a8ba8b04b4044d
student <student@eda-bc-03.localdomain>
2021-01-08 11:42:33 +1300

    silvia

--- server.js
+++ server.js
@@ -6,4 +6,9 @@ server.get ("/compliment", (req, res) =>{
   res.send ("<h1>You look good!</h1>")
 })
 
+server.get ("/profile", (req, res) =>{
+  res.sendFile(__dirname + '/silvia.html')
+
+})
+
 module.exports = server
