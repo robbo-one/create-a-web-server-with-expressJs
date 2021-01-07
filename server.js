32d191f0466ab27aed08f7a270a91992fdac92e3
naomi-porter <73915562+naomi-porter@users.noreply.github.com>
2021-01-08 12:02:33 +1300

    Question 2 completed

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const server = express();
 
-
-
-server.get('/compliment', (request, response) => {
+// Question 1 - Give a compliment
+server.get("/compliment", (request, response) => {
   response.send("You have beautiful eyes");
 });
 
-
-
-
+//Question 2 - Silvia's profile
+server.get("/profile", (request, response) => {
+  response.sendFile(__dirname + "/silvia.html");
+});
 
 module.exports = server;
