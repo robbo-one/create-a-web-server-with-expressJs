9e8990cbb53a934952b0373f8be9062c0cd8fb71
student <student@eda-bc-06.localdomain>
2021-01-08 12:00:01 +1300

    Added query stuff.

--- server.js
+++ server.js
@@ -9,5 +9,12 @@ server.get('/compliment', (req, res) => {
 })
 
 server.get('/profile', (req, res) => {
-  res.sendFile(__dirname + '/silvia.html')
+  console.log(req.query.name)
+  if(req.query.name == "silvia"){
+    res.sendFile(__dirname + '/silvia.html')
+  } else if(req.query.name == "sampson"){
+    res.sendFile(__dirname + '/sampson.html')
+  }
+
+  
 })
