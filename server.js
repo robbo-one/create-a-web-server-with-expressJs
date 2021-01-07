cd2e6790cbcb531aeb7d80dbcbed28e00f5ce9e4
student <student@eda-bc-06.localdomain>
2021-01-08 11:45:11 +1300

    add silvias profile

--- server.js
+++ server.js
@@ -7,3 +7,7 @@ module.exports = server
 server.get('/compliment', (req, res) => {
   res.send('Nice hair!')
 })
+
+server.get('/profile', (req, res) => {
+  res.sendFile(__dirname + '/silvia.html')
+})
