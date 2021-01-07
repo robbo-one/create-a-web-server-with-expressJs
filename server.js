b3cbc05f9950bffff252c537f09b6fe9f8b5d374
student <student@eda-bc-06.localdomain>
2021-01-08 12:07:49 +1300

    add profiles params

--- server.js
+++ server.js
@@ -9,12 +9,20 @@ server.get('/compliment', (req, res) => {
 })
 
 server.get('/profile', (req, res) => {
-  console.log(req.query.name)
   if(req.query.name == "silvia"){
     res.sendFile(__dirname + '/silvia.html')
   } else if(req.query.name == "sampson"){
     res.sendFile(__dirname + '/sampson.html')
   }
-
-  
 })
+
+
+server.get('/profiles/:id', (req, res) => {
+  if(req.params.id == 1){
+    res.sendFile(__dirname + '/silvia.html')
+  }
+  else if (req.params.id == 2){
+    res.sendFile(__dirname + '/sampson.html')
+  }
+})
+
