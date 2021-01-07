3c92cecaf9cd250490b2f7a71219fd1f62e52c44
student <student@eda-bc-05.localdomain>
2021-01-08 12:33:43 +1300

    step 4 complete

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ server.get('/', (request, response) => {
   response.send('hi')
 })
 
-server.get('/profile', (request, response) => {
-  response.sendFile(__dirname + '/silvia.html')
+// server.get('/profile', (request, response) => {
+//   response.sendFile(__dirname + '/silvia.html')
+// })
+
+server.get('/profile/1', (req, res) => {
+  res.sendFile(__dirname + '/silvia.html')
+})
+
+server.get('/profile/2', (req, res) => {
+  res.sendFile(__dirname + '/sampson.html')
 })
