745083530771ba7dcceed3c6cab34ac98e474a49
Julia Kurganskaia <17925683+julia-kurganskaia@users.noreply.github.com>
2021-01-09 11:51:35 +1300

    Question 4 complited

--- server.js
+++ server.js
@@ -2,16 +2,24 @@ const express = require("express");
 
 const server = express();
 
-// Question 1 - Give a compliment
+// Give a compliment
 server.get("/compliment", (request, response) => {
   response.send("You have beautiful eyes");
 });
 
-//Question 2 - Silvia's profile
-server.get("/profile", (request, response) => {
-  if (request.query.name === "silvia") {
+// profiles
+// server.get("/profile", (request, response) => {
+//   if (request.query.name === "silvia") {
+//     response.sendFile(__dirname + "/silvia.html");
+//   } else if (request.query.name === "sampson") {
+//     response.sendFile(__dirname + "/sampson.html");
+//   }
+// });
+
+server.get("/profiles/:id", (request, response) => {
+  if (request.params.id == 1) {
     response.sendFile(__dirname + "/silvia.html");
-  } else if (request.query.name === "sampson") {
+  } else if (request.params.id == 2) {
     response.sendFile(__dirname + "/sampson.html");
   }
 });
