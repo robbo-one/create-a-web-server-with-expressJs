fcb2d0cc278c7a6f9e4c5c69c103f2e552aafbad
Mike Parkin <michael.parkin1988@gmail.com>
2021-01-09 12:00:46 +1300

    server returning /compliment

--- server.js
+++ server.js
@@ -2,4 +2,10 @@ const express = require('express')
 
 const server = express()
 
+//routes
+
+server.get('/compliment', (req, res) => {
+    res.send('<h2>hi, you look cute and snuggly</h2>')
+})
+
 module.exports = server
