a132bc6e23813df9ae9be7f8a7ee6eacd8f33ec7
naomi-porter <73915562+naomi-porter@users.noreply.github.com>
2021-01-09 13:38:33 +1300

    Question 6 Completed

--- server.js
+++ server.js
@@ -7,15 +7,16 @@ server.get("/compliment", (request, response) => {
   response.send("You have beautiful eyes");
 });
 
-// profiles
-// server.get("/profile", (request, response) => {
-//   if (request.query.name === "silvia") {
-//     response.sendFile(__dirname + "/silvia.html");
-//   } else if (request.query.name === "sampson") {
-//     response.sendFile(__dirname + "/sampson.html");
-//   }
-// });
+// Profile query
+server.get("/profile", (request, response) => {
+  if (request.query.name === "silvia") {
+    response.sendFile(__dirname + "/silvia.html");
+  } else if (request.query.name === "sampson") {
+    response.sendFile(__dirname + "/sampson.html");
+  }
+});
 
+// Profiles parameters
 server.get("/profiles/:id", (request, response) => {
   if (request.params.id == 1) {
     response.sendFile(__dirname + "/silvia.html");
