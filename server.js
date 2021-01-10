d029fad7d4ebbdc6e442ea5a1b029958ca74f4ed
jack-bennett <bennettjack44@gmail.com>
2021-01-10 13:41:38 +1300

    completed URL parameter

--- server.js
+++ server.js
@@ -2,4 +2,26 @@ const express = require('express')
 
 const server = express()
 
+const PORT = 3000
+
+server.get('/compliment', (req, res) => {
+    res.send('<h1>Kia Ora</h1>')
+})
+
+server.get('/profile', (req, res) => {
+    if (req.query.name === 'sampson') {
+        res.sendFile(__dirname + '/sampson.html')
+    } else if (req.query.name === 'silvia') {
+        res.sendFile(__dirname + '/silvia.html')
+    }
+})
+
+server.get('/profiles/:id', (req, res) => {
+    if (req.params.id === '1') {
+        res.sendFile(__dirname + '/silvia.html')
+    } else if (req.params.id === '2') {
+        res.sendFile(__dirname + '/sampson.html')
+    }
+})
+
 module.exports = server
