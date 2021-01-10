40624f9e4fb4b657e7667d40be5818e9f674ca26
Tom May <tommay90@gmail.com>
2021-01-10 17:14:25 +1300

    add stylez

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 const express = require('express')
 const server = express()
 
-
 module.exports = server
 
+//access to public folder in project
+server.use(express.static('public'))
+
+//first page for test
 server.get('/',(req,res) => {
    res.send("hi hi")
 })
 
+//You have a very
 server.get('/compliment',(req,res) => {
   res.send("<h1><i>Nice face</i></h1>")
 })
 
+//Route using a query (?)
+server.get('/profile',(req,res) => {
+  if (req.query.name == 'silvia'){
+  res.sendFile(__dirname + '/public/silvia.html')
+  } else if (req.query.name == 'sampson'){
+      res.sendFile(__dirname + '/public/sampson.html')
+  }
+})
+
+//Route using a parameter (:)
+server.get('/profiles/:id',(req,res) => {
+  if (req.params.id == 1){
+    res.sendFile(__dirname + '/public/silvia.html')
+  } else if (req.params.id == 2){
+      res.sendFile(__dirname + '/public/sampson.html')
+  }
+})
 
