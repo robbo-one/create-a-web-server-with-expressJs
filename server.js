18e99f7fbf08fb6cf83b19e973ce219f5d8738f4
Tom May <tommay90@gmail.com>
2021-01-10 18:11:09 +1300

    complete all tasks

--- server.js
+++ server.js
@@ -3,34 +3,43 @@ const server = express()
 
 module.exports = server
 
+//urlencoded middleware
+server.use(express.urlencoded({
+  extended: true
+}))
+
 //access to public folder in project
 server.use(express.static('public'))
 
 //first page for test
-server.get('/',(req,res) => {
-   res.send("hi hi")
+server.get('/', (req, res) => {
+  res.send("hi hi")
 })
 
 //You have a very
-server.get('/compliment',(req,res) => {
+server.get('/compliment', (req, res) => {
   res.send("<h1><i>Nice face</i></h1>")
 })
 
 //Route using a query (?)
-server.get('/profile',(req,res) => {
-  if (req.query.name == 'silvia'){
-  res.sendFile(__dirname + '/public/silvia.html')
-  } else if (req.query.name == 'sampson'){
-      res.sendFile(__dirname + '/public/sampson.html')
+server.get('/profile', (req, res) => {
+  if (req.query.name == 'silvia') {
+    res.sendFile(__dirname + '/public/silvia.html')
+  } else if (req.query.name == 'sampson') {
+    res.sendFile(__dirname + '/public/sampson.html')
   }
 })
 
 //Route using a parameter (:)
-server.get('/profiles/:id',(req,res) => {
-  if (req.params.id == 1){
+server.get('/profiles/:id', (req, res) => {
+  if (req.params.id == 1) {
     res.sendFile(__dirname + '/public/silvia.html')
-  } else if (req.params.id == 2){
-      res.sendFile(__dirname + '/public/sampson.html')
+  } else if (req.params.id == 2) {
+    res.sendFile(__dirname + '/public/sampson.html')
   }
 })
 
+//Get name, give compliment Route
+server.post('/named-compliment', (req,res) => {
+  res.send('You are a wonderful person ' + req.body.name + ' :)')
+}) 
