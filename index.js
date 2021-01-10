ab21f2493520bcd2b5945c52b957b07e7e690ae3
Rob <robsc801@gmail.com>
2021-01-10 15:35:52 +1300

    link css and test middleware

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const server = require('./server')
 
 const PORT = process.env.PORT || 3000
 
+
+
+
+// STARTING THE SERVER
 server.listen(PORT, function () {
   // eslint-disable-next-line no-console
   console.log('Server listening on PORT', PORT)
--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const server = express()
 
 module.exports = server
 
+
+//MIDDLEWARE:
+
+server.use(express.static('public'))  //This is making it look in th epublic folder to find the required files.
+//when using server.use it's saying here is an 
+//extra piece of information to add and use on the server
+
+
+//ROUTES:
+
+//localhost:3000
 server.get('/', (request, response) => {
   response.send('hi')
 })
@@ -13,10 +24,12 @@ server.get('/', (request, response) => {
 //   response.sendFile(__dirname + '/silvia.html')
 // })
 
+//localhost:3000/profiles/1    //fromFile
 server.get('/profiles/1', (req, res) => {
   res.sendFile(__dirname + '/silvia.html')
 })
 
+//localhost:3000/profiles/2
 server.get('/profiles/2', (req, res) => {
   res.sendFile(__dirname + '/sampson.html')
 })
